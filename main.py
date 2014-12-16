c8de30bad469d66d560654e4d5d19d125ec74ff2
bisenen <magaster@gmail.com>
2014-12-17 01:19:24 +0300

    Update main.py
    some fixes

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import urllib2
 import sqlite3
 import time
 
-db_filename = '/tmp/avito.db'
+db_filename = 'avito.db'
 
 query="wii"
 url="https://www.avito.ru/moskva/igry_pristavki_i_programmy/igrovye_pristavki?view=list&q={0}".format(query)
 host="https://www.avito.ru"
-
+new=[]
 
 
 def exsql(query):
@@ -41,7 +41,8 @@ def init_exist_db():
         query="""create table list (
                  id integer primary key autoincrement,
                  name text,
-                 price int
+                 price int,
+                 new int
                  );
                  """
         exsql(query)
@@ -62,7 +63,8 @@ def get(url1):
 
 
 def getItem(data):
-    time.sleep(5)
+    time.sleep(1)
+#    list_db = exsql("SELECT name FROM list;")
     list = data.split('<div id="yandex-direct-org"> <script>')[0].split('<div class="item ')
     if len(list) != 0:
         for i in list:
@@ -70,10 +72,17 @@ def getItem(data):
                 try:
                     item_url = host + i.split("\n")[5].split(" ")[3].replace('href="',"").replace('"',"")
                     item_price = i.split("price")[1].split("</p>")[0].split("\n")[1].replace("&nbsp;","").split("&#160")[0].strip()
-                    #print "insert"
-                    exsql("INSERT INTO list (name,price) VALUES ('{0}','{1}');".format(item_url, item_price))
+                    #print "X"
+                    try:
+                        if item_url in exsql("SELECT name FROM list WHERE name = '{0}'".format(item_url))[0]:
+                            #print exsql("SELECT name FROM list WHERE name = '{0}'".format(item_url))[0]
+                            exsql("UPDATE list SET new = 0 WHERE name = '{0}'".format(item_url))
+                        else:
+                            exsql("INSERT INTO list (name,price,new) VALUES ('{0}','{1}',1);".format(item_url, item_price))
+                    except IndexError:
+                        exsql("INSERT INTO list (name,price,new) VALUES ('{0}','{1}',1);".format(item_url, item_price))
                 except IndexError:
-                    #print "indexerr"
+                    print "indexerr"
                     pass
     else:
        print len(list)
@@ -82,8 +91,8 @@ def getItem(data):
 
 
 
-exsql('DELETE FROM list;')
 init_exist_db()
+
 for x in range(1,10):
     try:
         getItem(get(url+"&p={0}".format(x)))
@@ -95,7 +104,8 @@ for x in range(1,10):
 
 
 
-for i in exsql("SELECT price,name FROM list where price > 2000 and price < 4000 ORDER BY price;"):
+for i in exsql("SELECT price,name,new FROM list where price > 2000 and price < 4000 and new = 1 ORDER BY price;"):
     print i
+
 #for i in range(1,999)
 #    url + "&p={0}".format()
