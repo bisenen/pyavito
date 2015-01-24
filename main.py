9daee3acf37837d4f5a15988263546055b96526a
bisenen <magaster@gmail.com>
2015-01-25 00:36:46 +0300

    fix index errors

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+ # -*- coding: utf8 -*-
 import urllib2
 import sqlite3
 import time
 
 db_filename = 'avito.db'
 
-query="wii"
-url="https://www.avito.ru/moskva/igry_pristavki_i_programmy/igrovye_pristavki?view=list&q={0}".format(query)
+query="сервер"
+url="https://www.avito.ru/moskva?view=list&q={0}".format(query)
 host="https://www.avito.ru"
 new=[]
 
@@ -63,6 +64,7 @@ def get(url1):
 
 
 def getItem(data):
+    inum = 0
     time.sleep(1)
 #    list_db = exsql("SELECT name FROM list;")
     list = data.split('<div id="yandex-direct-org"> <script>')[0].split('<div class="item ')
@@ -70,19 +72,18 @@ def getItem(data):
         for i in list:
             if "price" in i:
                 try:
-                    item_url = host + i.split("\n")[5].split(" ")[3].replace('href="',"").replace('"',"")
+                    item_url = host + i.split("href")[2].split('"')[1]
                     item_price = i.split("price")[1].split("</p>")[0].split("\n")[1].replace("&nbsp;","").split("&#160")[0].strip()
-                    #print "X"
                     try:
                         if item_url in exsql("SELECT name FROM list WHERE name = '{0}'".format(item_url))[0]:
-                            #print exsql("SELECT name FROM list WHERE name = '{0}'".format(item_url))[0]
-                            #exsql("UPDATE list SET new = 0 WHERE name = '{0}'".format(item_url))
                             pass
                         else:
                             exsql("INSERT INTO list (name,price,new) VALUES ('{0}','{1}',1);".format(item_url, item_price))
                     except IndexError:
-                        exsql("INSERT INTO list (name,price,new) VALUES ('{0}','{1}',1);".format(item_url, item_price))
+                       exsql("INSERT INTO list (name,price,new) VALUES ('{0}','{1}',1);".format(item_url, item_price))
                 except IndexError:
+                    print("XXXXXXXXXX")
+                    print(i)
                     print "indexerr"
                     pass
     else:
@@ -94,13 +95,14 @@ def getItem(data):
 
 init_exist_db()
 exsql("UPDATE list SET new = 0;")
-for x in range(1,10):
+for x in range(1,100):
     try:
         getItem(get(url+"&p={0}".format(x)))
         print x
+        time.sleep(1)
     except IOError:
         break
 
-for i in exsql("SELECT price,name,new FROM list where price > 2000 and price < 4000 and new = 1 ORDER BY price;"):
+for i in exsql("SELECT price,name,new FROM list where price > 24000 and price < 30000 and new = 1 ORDER BY price;"):
     print i
-#TAG1
+
